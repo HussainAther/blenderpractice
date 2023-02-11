c53418573016f852efd6b4a26e92b273f3790a1a
Syed Hussain Ather <shussainather@gmail.com>
2023-02-10 22:14:09 -0500

    def potential

--- scripts/lagrangianequilibrium.py
+++ scripts/lagrangianequilibrium.py
@@ -6,3 +6,13 @@ from scipy.optimize import fsolve
 import numpy as np
 from scipy.optimize import newton
 import matplotlib.pyplot as plt
+
+def dPotential1(r, *mus):
+    errflg = 0
+    #Unpack tuple
+    mu1, mu2 = mus
+    T1 = 1. - r
+    T2 = T1 - 1.0/T1**2
+    T3 = r - 1.0/r**2
+    dU = mu1*T2 - mu2*T3 # Equation 3.73
+    return dU
