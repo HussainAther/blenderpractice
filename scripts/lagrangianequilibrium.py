4f07a7b759e3138f0a59defb565750924d090565
Syed Hussain Ather <shussainather@gmail.com>
2023-02-10 22:15:27 -0500

    dpotential

--- scripts/lagrangianequilibrium.py
+++ scripts/lagrangianequilibrium.py
@@ -26,3 +26,13 @@ def dPotential2(r, *mus):
     T3 = r - 1.0/r**2;
     dU = mu1*T2 + mu2*T3 #Equation 3.85
     return dU
+
+def dPotential3(r, *mus):
+    errflg = 0
+    #Unpack tuple
+    mu1, mu2 = mus
+    T1 = 1. + r
+    T2 = r - 1.0/r**2
+    T3 = T1 - 1.0/T1**2
+    dU = mu1*T2 + mu2*T3 # Equation 3.90
+    return dU
