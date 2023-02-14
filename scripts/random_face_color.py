c6b0461b310e1db70e5ceda031b87bf209092381
Syed Hussain Ather <shussainather@gmail.com>
2023-02-14 13:24:40 -0500

    register and finish

--- scripts/random_face_color.py
+++ scripts/random_face_color.py
@@ -37,3 +37,60 @@ getUnusedRandoms = lambda : [ x for x in bpy.data.materials
 def clearMaterialSlots( ob ):
     while len( ob.material_slots ) > 0:
         bpy.ops.object.material_slot_remove()
+
+def removeUnusedRandoms():
+    unusedRandoms = getUnusedRandoms()
+    for mat in unusedRandoms:
+        bpy.data.materials.remove( mat )
+
+class RemoveUnusedRandomOp( bpy.types.Operator ):
+    bl_label = "Remove Unused Randoms"
+    bl_options = { 'REGISTER'}
+    bl_idname = "material.remove_unusedmats"
+
+    def execute( self, context ):
+    removeUnusedRandoms()
+    return {'FINISHED'}
+
+class RandomMatOp( bpy.types.Operator ):
+    bl_label = "Random Face Materials"
+    bl_idname = "material.randommat"
+    bl_options = { 'REGISTER', 'UNDO' }
+
+    def execute( self, context ):
+        ob = context.active_object
+        clearMaterialSlots( ob )
+        removeUnusedRandoms()
+        makeMaterials( ob )
+        eassignMats2Ob( ob )
+        return {'FINISHED'}
+
+    @classmethod
+    def poll( self, context ):
+        ob = context.active_object
+        return ob != None and ob.select
+
+class RandomMatPanel( bpy.types.Panel ):
+    bl_label = "Random Mat Panel"
+    bl_region_type = "TOOLS"
+    bl_space_type = "VIEW_3D"
+    def draw( self, context ):
+        self.layout.row().operator( "material.randommat" )
+        row = self.layout.row()
+        self.layout.row().operator( "material.remove_unusedmats" )
+        matCount = len( getUnusedRandoms() )
+        countLabel = "Unused Random Materials: %d" % matCount
+        self.layout.row().label( countLabel )
+
+def register():
+    bpy.utils.register_class( RemoveUnusedRandomOp )
+    bpy.utils.register_class( RandomMatOp )
+    bpy.utils.register_class( RandomMatPanel )
+
+def unregister():
+    bpy.utils.unregister_class( RandomMatPanel )
+    bpy.utils.unregister_class( RandomMatOp )
+    bpy.utils.unregister_class( RemoveUnusedRandomOp )
+
+if __name__ == '__main__':
+    register()
